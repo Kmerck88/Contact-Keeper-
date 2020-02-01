281dfd80084408eb8a8fbe6c700c3ada9ac23d0c
Kmerck88 <39713216+Kmerck88@users.noreply.github.com>
2020-02-01 07:50:40 -0500

    Express server with initial routes

---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const express = require('express'); 
+const router = express.Router(); 
+
+//@route    GET api/auth
+//@desc     Get logged in user 
+//@access   private
+router.get('/', (req, res)=> { 
+  res.send('Get logged in user');
+}); 
+
+//@route    Post api/auth
+//@desc     Auth user & get token 
+//@access   public
+router.post('/', (req, res) => { 
+  res.send('log in user');
+}); 
+
+module.exports = router; 
