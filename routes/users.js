a0480b5a3951448b0369a16ab9c5a03ed40bf51d
Kmerck88 <39713216+Kmerck88@users.noreply.github.com>
2020-02-03 15:55:26 -0500

    User Model & Validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,31 @@
-const express = require('express'); 
-const router = express.Router(); 
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 //@route    Post api/users
-//@desc     Register a user 
+//@desc     Register a user
 //@access   Public
-router.post('/', (req, res)=> { 
-  res.send('Register a user');
-}); 
+router.post(
+  '/',
+  [
+    check('name', 'Please add name')
+      .not()
+      .isEmpty(),
+      check('email', 'Please include valid email').isEmail(),
+      check(
+        'password', 
+        'please enter password with 6 or more characters'
+        ).isLength({min:6})
+  ],
+  
+  (req, res) => {
+    const errors = validationResult(req); if(!errors.isEmpty()){ 
+      return res.status(400).json({errors: errors.array()})
+    }
+    res.send('passed');
+  }
+);
 
-module.exports = router; 
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db'); 
 
 const app = express();
 
-app.get('/', (req, res) =>
+//Connect Database
+connectDB();
+
+// Init Middleware 
+app.use(express.json({extended: false}));
+
+app.get('/', (_req, res) =>
   res.json({ msg: 'Welcome to the ContactKeeper API' })
 );
 
