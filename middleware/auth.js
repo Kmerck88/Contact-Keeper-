87a024ea21e67a9e53cea9202fff967e5933bba9
Kmerck88 <39713216+Kmerck88@users.noreply.github.com>
2020-02-10 17:47:27 -0500

    fixed logged in user bug

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const config =require('config');
 
 module.exports = function(req, res, next){ 
   //Get token from header 
-  const token = req.header('x-auth-tokenn'); 
+  const token = req.header('x-auth-token'); 
   
   //Check if not token 
   if(!token) { 
