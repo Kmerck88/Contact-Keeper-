2608cc5e41e2eb2921035f226467aeaa46789067
Kmerck88 <39713216+Kmerck88@users.noreply.github.com>
2020-03-08 14:11:16 -0400

    Client Stuff

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <Link>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+        </ul>
+      </Link>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: propTypes.string.isRequired,
+  icon: propTypes.string
+};
+
+Navbar.defaultProps = {
+  title: 'Contact Keeper',
+  icon: 'fas fa-id-card-alt'
+};
+export default Navbar;
