c3193460d21eb0589da8297d4bbb2438b8c48c16
Kmerck88 <39713216+Kmerck88@users.noreply.github.com>
2020-03-17 16:11:23 -0400

    ContactItems

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Contacts from '../contacts/Contacts'
 
 const Home = () => {
   return (
+    <div className="grid-2">
     <div>
-    <h1>Home</h1>
+      {/*ContactForm*/}
+    </div>
+    <div>
+      <Contacts />
+    </div>
     </div>
   )
 }
