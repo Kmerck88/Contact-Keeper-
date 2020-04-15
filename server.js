5daf6bf802a841a16b765f611aa24f68229b1d8d
Kmerck88 <39713216+Kmerck88@users.noreply.github.com>
2020-04-15 16:08:21 -0400

    make it better

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 
+
 // Serve static assets in productions 
 if(process.env.NODE_ENV === 'production') { 
   // Set static folder 
@@ -23,7 +24,6 @@ if(process.env.NODE_ENV === 'production') {
   app.get("*", (req,res) =>res.sendFile(path.resolve(__dirname, 'client', 'build', 
   'index.html')));
 }
-
 
 const PORT = process.env.PORT || 5000;
 
